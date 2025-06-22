21bb9e6e18d6f5094c8b7fd4047be046078beb91
Amine <louati773@gmail.com>
2025-06-22 03:21:49 +0200

    Added missing db folder

---
+++ book.sql
@@ -0,0 +1,354 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 22 juin 2025 à 03:18
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `book`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `rating` decimal(2,1) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `author`, `description`, `price`, `rating`, `image`, `category_id`) VALUES
+(10, 'titile', 'jk Rowling', 'no description available fo now ', '250.00', NULL, '../uploads/685756c39fce7_1984.jpg', 1),
+(12, 'test', 'test', 'description test', '50.00', NULL, '../uploads/685756cda08d2_atlasparadox.webp', 8),
+(13, 'test2', 'test2', 'test 2 desc', '40.00', NULL, '../uploads/685756d78c0f9_popular.jpg', 4),
+(14, 'testtest', 'testtest', 'testtest desc', '80.00', NULL, '../uploads/685756e8a4331_7.jpg', 6),
+(15, 'teeest', 'testttttttt', 'testttttttttttttt', '60.00', NULL, '../uploads/685756e059e88_3.jpg', 10),
+(16, 'testtesttttttttttest', 'testtesttttttttttest', 'tetstesttesttttttttttest', '300.00', NULL, '../uploads/68575711ef64e_pride.jpg', 3),
+(17, 'testtesssst', 'testesttttttttttttttttt', 'testtesttttttttttesttesttesttttttttttest', '50.00', NULL, '../uploads/68575707e89c4_science.jpg', 9),
+(18, 'BOOK', 'authorr', 'author book', '500.00', NULL, '../uploads/685756ff9657d_harry.webp', 7),
+(19, 'new', 'arrival', 'new arrival desc', '200.00', NULL, '../uploads/685756f7117c2_new.webp', 2),
+(24, 'new book ', 'new', 'this new book  is by me', '200.00', NULL, '../uploads/685756f0244bf_children.webp', 7),
+(25, 'd', 'fsvs', 'kvsk mvm', '40.00', NULL, '../uploads/68574cb017cca_aot.avif', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `created_at`) VALUES
+(1, 5, '2025-01-31 11:22:08'),
+(2, 10, '2025-01-31 14:45:49'),
+(3, 11, '2025-01-31 23:35:12'),
+(4, 15, '2025-02-01 14:01:25'),
+(5, 16, '2025-06-21 23:26:08');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cart_items`
+--
+
+CREATE TABLE `cart_items` (
+  `id` int(11) NOT NULL,
+  `cart_id` int(11) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `cart_items`
+--
+
+INSERT INTO `cart_items` (`id`, `cart_id`, `book_id`, `quantity`) VALUES
+(28, 1, 13, 1),
+(30, 1, 14, 1),
+(32, 1, 12, 1),
+(33, 1, 17, 1),
+(34, 1, 15, 1),
+(36, 2, 10, 1),
+(39, 1, 19, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Bestsellers'),
+(8, 'Children'),
+(4, 'Featured'),
+(6, 'Fiction'),
+(10, 'History'),
+(11, 'Manga'),
+(3, 'Most Popular'),
+(2, 'New Arrivals'),
+(7, 'Non-Fiction'),
+(9, 'Science'),
+(5, 'Top Rated');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `status` varchar(50) DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `total`, `status`, `created_at`) VALUES
+(1, 10, '500.00', 'Pending', '2025-01-31 23:01:33'),
+(2, 11, '250.00', 'Pending', '2025-01-31 23:35:43'),
+(3, 15, '750.00', 'Pending', '2025-02-01 14:01:54'),
+(4, 16, '540.00', 'Pending', '2025-06-22 01:00:43');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `book_id`, `quantity`, `price`) VALUES
+(1, 1, 10, 2, '250.00'),
+(2, 2, 10, 1, '250.00'),
+(3, 3, 10, 3, '250.00'),
+(4, 4, 10, 2, '250.00'),
+(5, 4, 25, 1, '40.00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` varchar(50) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `role`) VALUES
+(5, 'userrr', 'user@user.user', '$2y$10$dwtVzwVeJrDGHEi2o7/L9uxfRF0oOsOcTpMJLgJ5BzWI784FwqF6i', '2025-01-26 11:37:40', 'user'),
+(8, 'admin', 'admin@admin', '$2y$10$Imj1NltL9J37kR47DDCoHeq0UdGpJ7aZkJjwZidJI/8ks8bCOSfaa', '2025-01-28 13:50:58', 'admin'),
+(10, 'aminn', 'mohamedamine.louati@iteam-univ.tn', '$2y$10$qvBo22VQzdo3IWMwTnwCuuYA82rLbewyaQ1n0pdeDGI3qe1FU4zDC', '2025-01-31 14:41:12', 'user'),
+(11, 'mohamed', 'mohamed@root.root', '$2y$10$bTQIbMbB/QyvoXhdFILb/OMFl1qKAz1mofKcrlYyQoPA8bnxefWZe', '2025-01-31 23:31:28', 'user'),
+(13, 'ibtisem', 'ibtisem@iteam.com', '$2y$10$6kzqlirY/JHcWqmLsu9DmOQ7d8RxX/ILXMuM1BX0OpKH6Y66KWTZO', '2025-02-01 13:36:47', 'user'),
+(14, 'uuserr', 'user@user', '$2y$10$nY4soQOgN1hfYQjdtOo1hO697AaPag9GfNP9jUfmNzsO3Hi7TAYyS', '2025-02-01 13:50:10', 'user'),
+(15, 'iteammm', 'iteam@iteam.iteam', '$2y$10$FgHHK7paLG093JWkFQwV5e6.6pkbSBDd.HhyiPgs0KJKZBWUTPVA2', '2025-02-01 14:00:53', 'user'),
+(16, 'loueti', 'test@test.com', '$2y$10$lc8/LHPHeROvaBnV6KJ1gebTb60e9evRUDdCLkR09YJcRsdg.N/.2', '2025-06-21 23:25:21', 'user');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Index pour la table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `cart_items`
+--
+ALTER TABLE `cart_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cart_id` (`cart_id`),
+  ADD KEY `cart_items_ibfk_2` (`book_id`);
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Index pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT pour la table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `cart_items`
+--
+ALTER TABLE `cart_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Contraintes pour la table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `cart_items`
+--
+ALTER TABLE `cart_items`
+  ADD CONSTRAINT `cart_items_ibfk_1` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`),
+  ADD CONSTRAINT `cart_items_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
